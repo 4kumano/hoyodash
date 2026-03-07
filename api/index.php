734ff4cb1da4895e305ded5232306b6ce71a1ceb
4kumano <54392161+4kumano@users.noreply.github.com>
2026-03-07 23:38:39 +0800

    up

--- api/index.php
+++ api/index.php
@@ -1,9 +1,4 @@
 <?php
 
-// Forward Vercel serverless requests to Laravel
-
-// Set the public path for static file serving
-$_SERVER['DOCUMENT_ROOT'] = __DIR__ . '/../public';
-$_ENV['APP_RUNNING_IN_VERCEL'] = true;
 
 require __DIR__ . '/../public/index.php';
